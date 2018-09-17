ae1680cac8bea47f1d93f6138df3d5816ef7073a
colabearwd <wz_jxnu@163.com>
2018-09-17 22:23:24 +0800

    question logic page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 from flask import Flask, render_template, request, redirect, url_for, session
 import config
-from models import User
+from models import User,Question
 from exts import db
 from decorator import login_required
 
@@ -84,7 +84,16 @@ def question():
     if request.method == 'GET':
         return render_template('question.html')
     else:
-        pass
+        title = request.form.get('title')
+        content = request.form.get('content')
+        question = Question(title=title , content=content)
+        user_id = session.get('user_id')
+        user = User.query.filter(User.id == user_id).first()
+        question.author = user
+        db.session.add(question)
+        db.session.commit()
+        return render_template('question.html')
+
 
 
 if __name__ == '__main__':
